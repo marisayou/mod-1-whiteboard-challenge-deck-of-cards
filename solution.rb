f672df7e1bff7d046a511bb9061313b6a8336814
Marisa You <marisa.you@gmail.com>
2020-10-19 09:37:11 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,35 @@
 class Deck
+    attr_accessor :cards
 
+    def initialize
+        self.make_deck
+    end
+
+    def make_deck
+        @cards = []
+        suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+        for i in 0...suits.length
+            for j in 0... ranks.length
+                new_card = Card.new(i, j)
+                @cards << new_card
+            end
+        end
+    end
+
+    def choose_card 
+        card = cards.sample
+        cards.delete(card)
+    end
 end
 
 class Card
+
+    attr_accessor :suit, :rank
+    def initialize(suit, rank)
+        @suit = suit
+        @rank = rank
+    end
 
 end
